ef876410b6c5faeec6ca32f976b460e08f6bb98c
Marian Plivelic <MarianPlivelic@gmail.com>
2025-01-03 12:50:47 +0100

    adjust treesitter

--- packages/nvim/dot-config/nvim/init.lua
+++ packages/nvim/dot-config/nvim/init.lua
@@ -174,10 +174,37 @@ vim.api.nvim_create_autocmd('BufWritePost', {
 })
 
 require'nvim-treesitter.configs'.setup{
-  ensure_installed = { "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "go", "gomod", "typescript", "javascript", "rust", "python" },
+  ensure_installed = {
+    'bash',
+    'css',
+    'go',
+    'gomod',
+    'html',
+    'javascript',
+    'json',
+    'lua',
+    'markdown',
+    'markdown_inline',
+    'python',
+    'query',
+    'rust',
+    'tmux',
+    'toml',
+    'tsx',
+    'typescript',
+    'vim',
+    'vimdoc',
+    'yaml'
+  },
   sync_install = false,
   auto_install = true,
-  highlight = {
-    enable = true
-  }
+  highlight = { enable = true },
+  additional_vim_regex_highlighting = false,
+  disable = function(lang, buf)
+      local max_filesize = 100 * 1024 -- 100 KB
+      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+      if ok and stats and stats.size > max_filesize then
+          return true
+      end
+  end,
 }

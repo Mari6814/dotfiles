7ab99c8a16e98a6468f5d8dd53d9096c128e6519
Marian Plivelic <MarianPlivelic@gmail.com>
2025-03-08 19:11:42 +0100

    add git action shortcuts to telescope

--- packages/nvim/dot-config/nvim/init.lua
+++ packages/nvim/dot-config/nvim/init.lua
@@ -92,6 +92,10 @@ require("lazy").setup({
             vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
             vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
             vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+            vim.keymap.set('n', '<leader>gb', builtin.git_branches, {})
+            vim.keymap.set('n', '<leader>gs', builtin.git_status, {})
+            vim.keymap.set('n', '<leader>gh', builtin.git_bcommits, {})
+            vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
         end
     },
     { 'github/copilot.vim' },

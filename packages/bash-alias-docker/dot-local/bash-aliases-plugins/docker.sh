b6acc1d89e68e015589de0208aa02a48fe8ba342
Marian Plivelic <MarianPlivelic@gmail.com>
2025-04-20 09:43:25 +0200

    add docker-exec

--- packages/bash-alias-docker/dot-local/bash-aliases-plugins/docker.sh
+++ packages/bash-alias-docker/dot-local/bash-aliases-plugins/docker.sh
@@ -3,6 +3,17 @@
 alias d="docker"
 alias dc="docker compose"
 alias de="docker exec -it"
+function docker-exec() {
+  if [ "$#" -lt 1 ]; then
+    echo "Usage: docker-exec <command> <container_name>"
+    return 1
+  fi
+  if [ "$#" -lt 2 ]; then
+    echo "Usage: docker-exec \"$1\" <container_name>"
+    return 1
+  fi
+  docker exec -it "$2" "$1"
+}
 function docker-bash() {
   docker exec -it "$1" bash
 }
@@ -16,4 +27,6 @@ _docker_bash_autocomplete() {
 
 # Register the autocompletion function for docker-bash
 complete -F _docker_bash_autocomplete docker-bash
+
+complete -F _docker_bash_autocomplete docker-exec
 
